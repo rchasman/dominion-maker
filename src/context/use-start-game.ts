d5dd4a85b45603a169463515fdfa1c50d2ab3d8f
Roey D. Chasman <rchasman@gmail.com>
2025-12-19 14:22:41 +1100

    RDC - Remove redundant setState call in useStartGame
    Remove setGameState call since new engine state will be set
    by the subsequent setEngine call's effect.

--- src/context/use-start-game.ts
+++ src/context/use-start-game.ts
@@ -43,7 +43,6 @@ export function useStartGame(
     setLLMLogs([]);
     setPlayerStrategies({});
     setEvents([]);
-    setGameState(engine.getState());
 
     const newEngine = new DominionEngine();
     setEngine(newEngine);
